c4e155c73874ec57d951758457fd4b2cf71ae2f3
Facundo Barafani <facu1910@gmail.com>
2019-09-25 11:31:32 -0300

    Add Index Excercice

---
+++ Third Term/IndexEx.sql
@@ -0,0 +1,37 @@
+-- Ex 1
+
+RESET QUERY CACHE;
+
+SELECT SQL_NO_CACHE *
+	FROM address a
+	INNER JOIN city USING(city_id)
+	INNER JOIN country USING(country_id)
+	WHERE a.postal_code IN (35200, 17886);
+	
+CREATE INDEX postalCode ON address(postal_code);
+
+DROP INDEX postalCode ON address;
+
+-- Ex 2
+
+SELECT *
+	FROM actor
+	WHERE first_name LIKE '%PENELOPE%';
+
+SELECT *
+	FROM actor
+	WHERE last_name LIKE '%ALLEN%';
+	
+-- Ex 3
+
+SELECT description
+	FROM film
+	WHERE description LIKE '%Epic%';
+	
+CREATE FULLTEXT INDEX filmDescription ON film(description);
+
+DROP INDEX filmDescription ON film;
+
+SELECT description
+	FROM film
+	WHERE MATCH(description) AGAINST('Epic');
